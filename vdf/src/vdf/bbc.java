bd2d8876b169c5c4129816cb27e7f56af36f313c
hanxiao <hanxiao@hanxiao>
2016-11-12 00:43:57 +0100

    lll

--- vdf/src/vdf/bbc.java
+++ vdf/src/vdf/bbc.java
@@ -3,7 +3,7 @@ package vdf;
 public class bbc {
 
 	public static void main(String[] args) {
-			System.out.println("hello workd");
+			System.out.println("hello wdddorkd");
 			System.out.println("hello workd");
 	}
 
