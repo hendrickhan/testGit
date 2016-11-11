4c9794227c902077dabe441920e90df57c6e579b
hanxiao <hanxiao@hanxiao>
2016-11-12 00:54:12 +0100

    dcsdc

--- vdf/src/vdf/bbc.java
+++ vdf/src/vdf/bbc.java
@@ -4,7 +4,7 @@ public class bbc {
 
 	public static void main(String[] args) {
 			System.out.println("hello wdddorkd");
-			System.out.println("hello worksssd");
+			System.out.println("hello wfrweorksssd");
 	}
 
 }
