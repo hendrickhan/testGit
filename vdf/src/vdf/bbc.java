488d9610c5b3da8ede7541d9fcfd086f188a05fd
hanxiao <hanxiao@hanxiao>
2016-11-12 00:44:53 +0100

    sss

--- vdf/src/vdf/bbc.java
+++ vdf/src/vdf/bbc.java
@@ -4,7 +4,7 @@ public class bbc {
 
 	public static void main(String[] args) {
 			System.out.println("hello wdddorkd");
-			System.out.println("hello workd");
+			System.out.println("hello worksssd");
 	}
 
 }
