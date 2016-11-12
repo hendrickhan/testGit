487481ab03321cd5bb0469420409c52278352ccc
hanxiao <hanxiao@hanxiao>
2016-11-12 01:08:25 +0100

    hjhm,

--- vdf/src/vdf/bbc.java
+++ vdf/src/vdf/bbc.java
@@ -3,8 +3,10 @@ package vdf;
 public class bbc {
 
 	public static void main(String[] args) {
-			System.out.println("hello wdddeeorkd");
-			System.out.println("hello wfrweoreksssd");
+		System.out.println("hello wdddeeorkd");
+		System.out.println("hello wfrweoreksssd");
+
+		System.out.println("hello wfrweoreksssd");
 	}
 
 }
